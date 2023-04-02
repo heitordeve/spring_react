6d85b640a8bef0f76cf69c7bc35469639b9f46f1
heitor.developer@outlook.com <heitor.souza@kimmais.com.br>
2023-04-02 18:19:57 -0300

    Valided requisition Model

--- src/main/java/br/com/api/produtos/model/RespostaModelo.java
+++ src/main/java/br/com/api/produtos/model/RespostaModelo.java
@@ -1,4 +1,14 @@
 package br.com.api.produtos.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
 public class RespostaModelo {
+
+    private String mensagem;
+
 }
