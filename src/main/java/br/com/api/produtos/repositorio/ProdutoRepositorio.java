f03ad0fa638ce2fafb0c50be6958afc9aadf19e0
heitor.developer@outlook.com <heitor.souza@kimmais.com.br>
2023-04-02 18:35:30 -0300

    Created service for listening products

--- src/main/java/br/com/api/produtos/repositorio/ProdutoRepositorio.java
+++ src/main/java/br/com/api/produtos/repositorio/ProdutoRepositorio.java
@@ -1,4 +1,6 @@
 package br.com.api.produtos.repositorio;
 
+import br.com.api.produtos.model.ProdutoModelo;
+
 public interface ProdutoRepositorio {
 }
--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,4 +1,20 @@
 package br.com.api.produtos.servico;
 
+
+import br.com.api.produtos.model.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoServico {
+
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    //Método para listar todos os produtos
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
+
 }
