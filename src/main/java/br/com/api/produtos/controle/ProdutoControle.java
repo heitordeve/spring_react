bbbd53d78b4ef653e6822ed34e0b73a7c81d79df
heitor.developer@outlook.com <heitor.souza@kimmais.com.br>
2023-04-03 15:43:40 -0300

    Created Route alter products

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -4,10 +4,7 @@ import br.com.api.produtos.model.ProdutoModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProdutoControle {
@@ -18,6 +15,11 @@ public class ProdutoControle {
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
         return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
     }
 
     @GetMapping("/listar")
