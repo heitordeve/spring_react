de0da898a52fa63e97a70c6a4e926f48730e6af3
heitor.developer@outlook.com <heitor.souza@kimmais.com.br>
2023-04-02 21:07:35 -0300

    Created Service store products

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -2,8 +2,11 @@ package br.com.api.produtos.servico;
 
 
 import br.com.api.produtos.model.ProdutoModelo;
+import br.com.api.produtos.model.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
@@ -13,9 +16,25 @@ public class ProdutoServico{
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
+
     //Método para listar todos os produtos
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
+    }
+
+    //Método para cadastrar produto
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setMensagem("O nome do marca é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+        }
     }
 
 }
